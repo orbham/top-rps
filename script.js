1df25bff6551cbc178b436362e9c358f3d9933fc
Hayden Truax <haydentrieux@gmail.com>
2024-11-07 00:32:55 -0800

    Added random index and human choice functions

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+function getComputerChoice() {
+    const options = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * options.length);
+    return options[randomIndex];
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function getHumanChoice(player, computer) {
+    return (
+            (player === "Rock" && computer === "Scissors") ||
+            (player === "Scissors" && computer === "Paper")||
+            (player === "Paper" && computer === "Rock")
+        );
+}
+
+getHumanChoice();
